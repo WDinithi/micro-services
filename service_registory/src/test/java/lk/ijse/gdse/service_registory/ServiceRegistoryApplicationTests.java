d8f4153d5f7997404d4432fa79a52f2188fa5857
Dinithi Wijewardana <123625900+WDinithi@users.noreply.github.com>
2024-07-09 19:55:39 +0530

    Add files via upload

---
+++ service_registory/src/test/java/lk/ijse/gdse/service_registory/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.service_registory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
